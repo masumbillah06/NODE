6518566ffeeb8df8883a5186ce46012b479b42f9
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2024-11-28 19:51:21 +0600

    test api change

--- dbconnect.js
+++ dbconnect.js
@@ -1,18 +1,18 @@
 import mysql from 'mysql';
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "students course registration system"
+	host: 'localhost',
+	user: 'root',
+	password: '1234',
+	database: 'students course registration system',
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to the database as id', db.threadId);
+	if (err) {
+		console.error('Error connecting to the database:', err.stack);
+		return;
+	}
+	console.log('Connected to the database as id', db.threadId);
 });
 
 export default db;
