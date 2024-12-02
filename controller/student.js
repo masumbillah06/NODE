71e945162689273f857c3038a9190f541a4b5a6f
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2024-12-02 14:51:02 +0600

    Improve createStudent Code in Controller

--- controller/student.js
+++ controller/student.js
@@ -10,7 +10,6 @@ export const allStudent = (req, res) => {
 	});
 };
 
-
 //==============================================================
 //Shows a single student
 export const singleStudent = (req, res) => {
@@ -23,37 +22,43 @@ export const singleStudent = (req, res) => {
 	});
 };
 
-
 //==============================================================
 //Inserts datas of a new student
 export const createStudent = (req, res) => {
-	const query = `INSERT INTO Students (student_id, name, email, phone, batch_no, address) VALUES ('${req.body.student_id}', '${req.body.name}', '${req.body.email}', '${req.body.phone}', '${req.body.batch_no}', '${req.body.address}');`;
+	const query = `
+        INSERT INTO Students (student_id, name, email, phone, batch_no, address) 
+        VALUES (?, ?, ?, ?, ?, ?);
+    `;
 
-	db.query(query, (err, data) => {
-		if (err) return res.status(500).json(err);
+	const { student_id, name, email, phone, batch_no, address } = req.body;
 
-		return res.status(201).json(req.body);
-	});
+	db.query(
+		query,
+		[student_id, name, email, phone, batch_no, address],
+		(err, data) => {
+			if (err) return res.status(500).json(err);
+
+			return res.status(201).json(req.body);
+		}
+	);
 };
-
 
 //==============================================================
 //Deletes datas of a student
 export const deleteAStudent = (req, res) => {
-    const query = `DELETE FROM students WHERE student_id = '${req.params.id}';`;
-    
+	const query = `DELETE FROM students WHERE student_id = '${req.params.id}';`;
+
 	db.query(query, (err, data) => {
-        if (err) return res.status(500).json(err);
+		if (err) return res.status(500).json(err);
 
 		return res.status(204).json(data);
-    });
-}
-
+	});
+};
 
 //==============================================================
 //Updates datas of a student
 export const updateStudent = (req, res) => {
-    const query = `UPDATE Students 
+	const query = `UPDATE Students 
         SET 
             name = '${req.body.name}', 
             email = '${req.body.email}', 
@@ -63,11 +68,12 @@ export const updateStudent = (req, res) => {
         WHERE student_id = '${req.body.student_id}';
     `;
 
-    db.query(query, (err, data) => {
-        if (err) return res.status(500).json(err);
+	db.query(query, (err, data) => {
+		if (err) return res.status(500).json(err);
 
-        return res.status(200).json({ message: "Student updated successfully", student: req.body });
-    });
+		return res.status(200).json({
+			message: 'Student updated successfully',
+			student: req.body,
+		});
+	});
 };
-
-
