0330d881cdfa1e419dcb2a836e592c57b680adaf
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2024-12-09 22:23:32 +0600

    fixed

--- controller/course.js
+++ controller/course.js
@@ -2,27 +2,25 @@ import db from './../dbconnect.js';
 
 //=================================================
 // Shows all courses
-export const  allCourse = (req, res) => {
-    db.query("SELECT * FROM courses;", (err, data) => {
-        if (err) return res.status(500).json(err);
+export const allCourse = (req, res) => {
+	db.query('SELECT * FROM courses;', (err, data) => {
+		if (err) return res.status(500).json(err);
 
 		return res.status(200).json(data);
-    });
-}
-
+	});
+};
 
 //=================================================
 // Shows a single course
 export const singleCourse = (req, res) => {
-    const query = `SELECT * FROM courses WHERE course_id = '${req.params.id}';`;
+	const query = `SELECT * FROM courses WHERE course_id = '${req.params.id}';`;
 
 	db.query(query, (err, data) => {
-        if (err) return res.status(500).json(err);
+		if (err) return res.status(500).json(err);
 
 		return res.status(200).json(data);
-    });
-}
-
+	});
+};
 
 //=================================================
 // Inserts datas of a single course
@@ -36,15 +34,19 @@ export const createCourse = (req, res) => {
 	});
 };
 
-
 //=================================================
 // Deletes datas of a single course
 export const deleteACourse = (req, res) => {
-    const query = `DELETE FROM courses WHERE course_id = '${req.params.id}';`;
-    
+	const query = `DELETE FROM courses WHERE course_id = '${req.params.id}';`;
+
 	db.query(query, (err, data) => {
-        if (err) return res.status(500).json({ error: "Can't delete a course which is already ongoing. Wait for the smemester to end!" });
+		if (err)
+			return res
+				.status(500)
+				.json({
+					error: "Can't delete a course which is already ongoing. Wait for the semester to end!",
+				});
 
 		return res.status(204).json(data);
-    });
-}
+	});
+};
